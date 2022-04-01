e1feea04cc82116675607d890b1fec087b336aa3
Evan Fredericks <evan.fredericks@gmail.com>
2022-04-01 11:32:26 -0500

    refactor recipe and ingredient controllers

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -61,7 +61,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Mono<Void> deleteById(String idToDelete) {
-        recipeReactiveRepository.deleteById(idToDelete).block();
+        recipeReactiveRepository.deleteById(idToDelete).subscribe();
 
         return Mono.empty();
     }
